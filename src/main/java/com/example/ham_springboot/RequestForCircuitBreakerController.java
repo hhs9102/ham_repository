a4e37078ddf55bb962b39dadaac52b83c933e805
hamhosik <hhs9102@naver.com>
2019-08-11 11:19:06 +0900

    springboot test / +CircuitBreaker 적용 및 테스트

---
+++ src/main/java/com/example/ham_springboot/RequestForCircuitBreakerController.java
@@ -0,0 +1,43 @@
+package com.example.ham_springboot;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.sound.midi.Soundbank;
+
+@EnableCircuitBreaker
+@RestController
+public class RequestForCircuitBreakerController {
+
+    @GetMapping("/consumer")
+    @HystrixCommand(fallbackMethod = "fallback" ,commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "4000") //요청 대기 시간
+            ,@HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5") //최소 요청 건수
+            ,@HystrixProperty(name = "- circuitBreaker.sleepWindowInMilliseconds", value = "5000") //서킷브레이커 지속시간
+    })
+    public String Consumer(@RequestParam String path){
+        System.out.println("log :: "+path);
+
+        ResponseEntity<String> entity = new RestTemplate().getForEntity("http://localhost:8080/" + path, String.class);
+        if(entity.getStatusCode() == HttpStatus.OK){
+            System.out.println("result :: "+entity.getBody());
+            return entity.getBody();
+        }else{
+            System.out.println("HttpStatus is not OK");
+        }
+
+        throw new RuntimeException("supplier is not OK");
+    }
+
+    private String fallback(String path){
+        System.out.println("called fallback method");
+        return "hello fallback";
+    }
+}
