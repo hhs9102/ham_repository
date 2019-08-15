33299c3227c92cce289770bf9589a35307021013
hamhosik <hhs9102@naver.com>
2019-08-15 10:34:40 +0900

    Error 매핑 추가

---
+++ src/main/java/me/ham/config/ErrorController.java
@@ -0,0 +1,28 @@
+package me.ham.config;
+
+import me.ham.config.exception.AppError;
+import me.ham.config.exception.SampleException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class ErrorController extends RuntimeException{
+
+    @GetMapping("/error/throw")
+    public String errorThrow() throws SampleException {
+        throw new SampleException();
+    }
+
+    @ExceptionHandler(SampleException.class)
+    public @ResponseBody AppError sampleError(SampleException sampleException){ //이 컨트롤러에서만 사용됨
+        AppError appError = new AppError();
+        appError.setMessage("error.app.key");
+        appError.setReasone("I don't know");
+        return appError;
+    }
+
+}
---
+++ src/main/java/me/ham/config/exception/SampleException.java
@@ -0,0 +1,4 @@
+package me.ham.config.exception;
+
+public class SampleException extends Throwable {
+}
