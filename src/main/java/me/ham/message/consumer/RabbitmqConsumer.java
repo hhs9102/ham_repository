e8c73fe13390658c3354854e3753a3bae5e460f4
hamhosik <hhs9102@naver.com>
2020-07-28 22:46:46 +0900

    springboot ssl 추가

--- src/main/java/me/ham/message/consumer/RabbitmqConsumer.java
+++ src/main/java/me/ham/message/consumer/RabbitmqConsumer.java
@@ -1,19 +1,24 @@
 package me.ham.message.consumer;
 
 import me.ham.user.model.User;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RabbitmqConsumer {
 
-    @RabbitListener(queues = "q.hsham")
+//    @RabbitListener(queues = "q.hsham")
     public void getFromMessageQueue(String message) {
+        printCurrentThreadName();
         System.out.println("Consuming :::"+message);
     }
 
-    @RabbitListener(queues = "q.person")
+//    @RabbitListener(queues = "q.person")
     public void getPersonFromMessageQueue(User user) {
+        printCurrentThreadName();
         System.out.println("Consuming :::"+user.toString());
+    }
+
+    private void printCurrentThreadName(){
+        System.out.println("Current thread::::"+Thread.currentThread().getName());
     }
 }
